42d61a9b280adafedf559c65de55a4926280bf8b
fatma166 <fatmaghareeb90@gmail.com>
2022-08-08 13:29:31 +0200

    first commit

---
+++ app/Models/Outdoor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Outdoor_attendance;
+class Outdoor extends Model
+{
+    //
+    // protected $fillable = [
+    //     'company_id','visit_type_id','title','adress','description','user_id','longi','lati','date','customer_id','created_by'
+    // ];
+        protected  $fillable=["title","lati","longi","adress","date","description","customer_id","visit_type_id","company_id","user_id","rate","created_by","created_at"];
+
+    public function report()
+    {
+        return $this->hasOne(Visit_report::class);
+    }
+
+     /**
+     * Get all of the comments for the Attendence
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function outdoor_attendances()
+    {
+        return $this->hasOne(Outdoor_attendance::class);
+    }
+}
---
+++ resources/views/client/search_client.blade.php
@@ -0,0 +1,43 @@
+                
+                    <div class="col-md-12 clients_result">
+                        <div class="table-responsive">
+                            <table class="table table-striped custom-table">
+                                <thead>
+                                    <tr>
+                                        
+                                        <th>{{__('trans.Name')}}</th>
+                                         <th>{{__('trans.Address')}}</th>
+                                        <th>{{__('trans.Contact Person')}}</th>
+                                        <th class="text-right">{{__('trans.Action')}}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @if(!empty($clients))
+                                @foreach($clients as $client)
+                                    <tr>
+                                        <td>    
+                                            
+                                            <a href="{{url('/admin/client-profile/'.$client->id)}}" class="avatar"></a>
+                                                                                             
+                                            <h2 class="table-avatar">    
+                                                 <!-- <a class="dropdown-item" href="#" data-href="{{ url('admin/client-edit/'.$client->id) }}" client-id="{{$client->id}}"
+                                                           data-toggle="modal" data-target="#edit_client">{{$client->name}}</a>-->
+                                                 <a  href="{{url('/admin/client-profile/'.$client->id)}}">{{$client->name}}</a>
+                                              
+                                            </h2>
+                                        </td>
+                                        <td style="overflow:hidden; display: inline-block;text-overflow: ellipsis;white-space: nowrap;width:200px;}">{{$client->address}}</td>
+                                        <td>{{$client->phone}}</td>
+                                        <td class="text-right">
+                                            <a class="btn btn-outline-success" href="#" data-href="{{ url('admin/client-edit/'.$client->id) }}" client-id="{{$client->id}}" data-toggle="modal" data-target="#edit_client"><i class="fa fa-pencil m-r-5"></i> {{__('trans.Edit')}}</a>
+                                           <!-- <a class="btn btn-outline-danger" href="#" data-toggle="modal" data-target="#delete_client" delete-id="{{$client->id}}"><i class="fa fa-trash-o m-r-5"></i> {{__('trans.Delete')}}</a>-->
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                @endif 
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+               
+                 {{ $clients->appends($_GET)->links() }}                
